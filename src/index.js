28e2d16748ffa75b09d9004f414fb8f01963685b
Nandkumar <nam.nandkumar@gmail.com>
2022-06-22 20:23:35 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//Components
+import App from './components/App';
+
+
+//styles
+import './assets/scss/survey.scss';
+
+
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+);
